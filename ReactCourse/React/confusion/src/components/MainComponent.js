dd4b02e21d7f6ae66b559cb86671cf62065a7f1e
Joe <jthompson.ucf@gmail.com>
2019-05-08 22:54:31 -0700

    React Course - Navigation with ReactRouter

--- ReactCourse/React/confusion/src/components/MainComponent.js
+++ ReactCourse/React/confusion/src/components/MainComponent.js
@@ -12,21 +12,22 @@ class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            dishes: DISHES,
-            selectedDish: null
+            dishes: DISHES
         }
     }
 
-    onDishSelect(dishId) {
-        this.setState({ selectedDish: dishId });
-    }
-
     render() {
+        const HomePage = () => {
+            return(<Home />);
+        }
         return (
             <div>
                 <Header />
-                <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+                <Switch>
+                    <Route path="/home" component={HomePage} />
+                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                    <Redirect to="/home" />
+                </Switch>
                 <Footer />
             </div>
         );
