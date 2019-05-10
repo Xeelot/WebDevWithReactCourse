9468ba3fa101a545bc858e274103d54413218a2c
Joe <jthompson.ucf@gmail.com>
2019-05-09 23:17:19 -0700

    React Course - Week 2 Assignment

--- ReactCourse/React/confusion/src/components/AboutComponent.js
+++ ReactCourse/React/confusion/src/components/AboutComponent.js
@@ -2,11 +2,28 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-function About(props) {
+function RenderLeader({leader}) {
+    return (
+        <div className="col-12 mt-4">
+            <Media tag="li">
+                <Media left>
+                    <Media object src={leader.image} alt={leader.name} />
+                </Media>
+                <Media body className="ml-5">
+                    <Media heading>{leader.name}</Media>
+                    <p>{leader.designation}</p>
+                    <p>{leader.description}</p>
+                </Media>
+            </Media>
+        </div>
+    );
+}
+
+const About = (props) => {
 
     const leaders = props.leaders.map((leader) => {
         return (
-            <p>Leader {leader.name}</p>
+            <RenderLeader leader={leader} key={leader.id} />
         );
     });
 
